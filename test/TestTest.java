6656e6bc45fee12c2854460ba3289d1caacca95c
asrimaspupah <asri.maspupah@yahoo.com>
2014-02-20 00:22:15 +0700

    test8

--- test/TestTest.java
+++ test/TestTest.java
@@ -1,10 +1,10 @@
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+//import static org.junit.Assert.assertTrue;
 
 public class TestTest {
-	@Test
-	public void testTest() {
-		assertTrue(1 == 1);
-	}
+//	@Test
+//	public void testTest() {
+	//	assertTrue(1 == 1);
+	//}
 }
 
